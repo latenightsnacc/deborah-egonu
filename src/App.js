15b8be52398b2e38d1392cc00f64db7733540384
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-06-22 10:20:31 +0100

    installed tailwindcss successfully

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-blue-500 capitalize font-bold text-5xl">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
