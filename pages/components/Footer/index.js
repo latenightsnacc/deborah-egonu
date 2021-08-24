839a697c5deb2737d1315c9ed02c6ebf66cccf44
Deborah <54311514+latenightsnacc@users.noreply.github.com>
2021-08-24 09:58:05 +0100

    made more changes

---
+++ pages/components/Footer/index.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="flex flex-row text-center h-14 items-center justify-center">
+            <p className="text-center font-light">Built with &hearts;</p>
+        </div>
+    )
+}
